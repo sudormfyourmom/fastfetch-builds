092af06b6d98fd932004224c0e182ac485367708
sudormfyourmom <albo9809@int.pl>
2026-05-11 19:17:34 +0200

    Add files via upload

---
+++ build/fastfetch_config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define FASTFETCH_PROJECT_NAME "fastfetch"
+#define FASTFETCH_PROJECT_VERSION "2.62.1"
+#define FASTFETCH_PROJECT_VERSION_MAJOR 2
+#define FASTFETCH_PROJECT_VERSION_MINOR 62
+#define FASTFETCH_PROJECT_VERSION_PATCH 1
+#define FASTFETCH_PROJECT_VERSION_GIT "2.62.0-86-g320e1bf4"
+#define FASTFETCH_PROJECT_VERSION_TWEAK "-86"
+#define FASTFETCH_PROJECT_VERSION_TWEAK_NUM 86
+#define FASTFETCH_PROJECT_CMAKE_BUILD_TYPE "RelWithDebInfo"
+#define FASTFETCH_PROJECT_HOMEPAGE_URL "https://github.com/fastfetch-cli/fastfetch"
+#define FASTFETCH_PROJECT_DESCRIPTION "Fast neofetch-like system information tool"
+#define FASTFETCH_PROJECT_LICENSE "MIT license"
+
+#define FASTFETCH_TARGET_DIR_ROOT "/data/data/com.termux/files/usr"
+#define FASTFETCH_TARGET_DIR_USR "/data/data/com.termux/files/usr"
+#define FASTFETCH_TARGET_DIR_HOME "/data/data/com.termux/files/home"
+#define FASTFETCH_TARGET_DIR_ETC "/data/data/com.termux/files/usr/etc"
+
+#define FASTFETCH_TARGET_DIR_INSTALL_SYSCONF "/data/data/com.termux/files/usr/etc"
